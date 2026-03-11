318d41f065cece96f16120400417d83bb2988f5f
sangmesh-18 <garibesangmesh@gmail.com>
2026-03-11 12:13:22 +0530

    feat: Add Redis configuration and connection setup using ioredis.

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,5 @@
 import Redis from "ioredis";
-
+ console.log("Redis password:", process.env.REDIS_PASSWORD);
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
